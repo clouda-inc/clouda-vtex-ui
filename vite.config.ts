a012a481b9064f84c4388aad2b5c4e2ec2f9fc80
jayendra88 <jayendra.k.a@gmail.com>
2026-01-17 22:16:50 -0600

    feature library 2

--- vite.config.ts
+++ vite.config.ts
@@ -24,26 +24,19 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      name: 'clouda-vtex-ui',
-      // the proper extensions will be added
-      fileName: 'clouda-vtex-ui'
+      formats: ['es', 'cjs'],
+      fileName: (format) =>
+        format === 'es'
+          ? 'clouda-vtex-ui.js'
+          : 'clouda-vtex-ui.cjs',
     },
     cssCodeSplit: true,
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'react/jsx-runtime'
-        },
-        assetFileNames: "style.css",
-      }
-    }
+        assetFileNames: 'style.css',
+      },
+    },
   },
   test: {
     projects: [{
